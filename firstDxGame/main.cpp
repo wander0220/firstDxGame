eb05c351ee428fed9506f9dcc2cecc97d18c3209
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-04-22 19:24:44 +0900

    Meshes에서 가져오기

--- firstDxGame/main.cpp
+++ firstDxGame/main.cpp
@@ -7,6 +7,12 @@
 #include <memory.h>
 #include <tchar.h>
 
+#include <mmsystem.h>
+#include <d3dx9.h>
+#pragma warning( disable : 4996 ) // disable deprecated warning 
+#include <strsafe.h>
+#pragma warning( default : 4996 )
+
 #include <iostream>
 
 #define MAX_LOADSTRING 100
@@ -15,6 +21,42 @@ HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
+HWND g_hWnd;
+LPDIRECT3D9         g_pD3D = NULL; // Used to create the D3DDevice
+LPDIRECT3DDEVICE9   g_pd3dDevice = NULL; // Our rendering device
+
+HRESULT InitD3D(HWND hWnd)
+{
+    // Create the D3D object.
+    if (NULL == (g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)))
+        return E_FAIL;
+
+    // Set up the structure used to create the D3DDevice. Since we are now
+    // using more complex geometry, we will create a device with a zbuffer.
+    D3DPRESENT_PARAMETERS d3dpp;
+    ZeroMemory(&d3dpp, sizeof(d3dpp));
+    d3dpp.Windowed = TRUE;
+    d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+    d3dpp.EnableAutoDepthStencil = TRUE;
+    d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+
+    // Create the D3DDevice
+    if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
+        D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+        &d3dpp, &g_pd3dDevice)))
+    {
+        return E_FAIL;
+    }
+
+    // Turn on the zbuffer
+    g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, TRUE);
+
+    // Turn on ambient lighting 
+    g_pd3dDevice->SetRenderState(D3DRS_AMBIENT, 0xffffffff);
+
+    return S_OK;
+}
 void Render() {
 
 }
@@ -68,11 +110,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     RegisterClassExW(&wcex);
 
-    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+    g_hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
+    InitD3D(g_hWnd);
+
+    ShowWindow(g_hWnd, nCmdShow);
+    UpdateWindow(g_hWnd);
 
     //HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINAPPWITHWIZARD));
 
